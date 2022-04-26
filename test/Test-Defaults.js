208b90cc48029979a1dd8a8dfe859e4815984f02
Maxi <maximilian.lohmann.hh@gmail.com>
2022-04-26 14:06:56 +0200

    Use a copied dataPoint inside calcPosition instead of modifying the chart data directly
    - Clean up signature of calcPosition
    
    - Add test case for disabled drag lines https://github.com/chrispahm/chartjs-plugin-dragdata/issues/96#issue-1215681376

--- test/Test-Defaults.js
+++ test/Test-Defaults.js
@@ -1,11 +1,15 @@
 module.exports = class TestSettings {
   constructor() {
     this.pointToDrag = { datasetIndex: 0, index: 0 }
+    this.draggingDisabled = false
     this.dragToPosition = (firstPointLocation,secondPointLocation) => {
       // array position 0 = x, 1 = y
       return [firstPointLocation[0],secondPointLocation[1]]
     }
-    this.checkEquality = (firstPointLocation,secondPointLocation, fileName) => {
+    this.checkEquality = (firstPointStartLocation, updatedLocations, fileName) => {
+      const firstPointLocation = updatedLocations[0]
+      const secondPointLocation = this.draggingDisabled ? firstPointStartLocation : updatedLocations[1]
+
       const diff = Math.abs( firstPointLocation.y - secondPointLocation.y );
       // allow for a 2 pixel tolerance
       if (diff > 2) {
