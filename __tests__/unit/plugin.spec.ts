3142193524b53f18f98fecaf3ac6d579768f3e73
artus9033 <artus9033@gmail.com>
2024-02-05 00:48:28 +0100

    fix: unit tests' mocks not working properly

--- __tests__/unit/plugin.spec.ts
+++ __tests__/unit/plugin.spec.ts
@@ -17,16 +17,20 @@ jest.mock("d3-drag", () => ({
 }));
 
 jest.mock("d3-selection", () => {
+	const callMock = jest
+		.fn((...args: any[]) => {
+			var callback = args[0];
+			args[0] = this;
+			(callback as any).apply(null, args);
+			return this;
+		})
+		.mockReturnThis();
+
 	return {
-		select: jest.fn().mockReturnThis(),
-		call: jest
-			.fn((...args: any[]) => {
-				var callback = args[0];
-				args[0] = this;
-				(callback as any).apply(null, args);
-				return this;
-			})
-			.mockReturnThis(),
+		select: jest.fn(() => ({
+			call: callMock,
+		})),
+		call: callMock,
 	};
 });
 
