79825522f285a3d843ce8bcbf0c58fefc4f28dee
artus9033 <artus9033@gmail.com>
2024-08-14 13:53:41 +0200

    docs: better formatting of custom interaction page code

--- pages/src/pages/line.page.ts
+++ pages/src/pages/line.page.ts
@@ -12,7 +12,7 @@ export default (({ isE2ETest }) => [
 		title: "Line (linear, custom interaction mode)",
 		fileName: "line-linear-custom-interaction.html",
 		isE2ETest: isE2ETest,
-		customJS: `Chart.Interaction.modes.maxValueCustomMode = ${maxValueCustomMode.toString()}`,
+		customJS: `Chart.Interaction.modes.maxValueCustomMode = ${maxValueCustomMode.toString()}\n`,
 		// make only the y-axis draggable in demo mode; E2E tests are not carried out for this page
 		demoOnlyDraggableAxis: "y",
 	}),
