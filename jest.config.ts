5538f993a03d0b7809bf1f9c523e14ccaa0f4c58
artus9033 <artus9033@gmail.com>
2024-08-15 01:05:42 +0200

    fix(test): move jest configuration options unallowed in project config to global

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,23 @@
 import type { Config } from "jest";
 
+import { testPathIgnorePatterns } from "./tests/__config__/commonJestConfig";
+
 // since jest projects don't inherit from root config, we instead use tests/__config__/commonJestConfig.ts
 // thus, here we want just to define projects' configs; see https://github.com/jestjs/jest/issues/10991
-const config: Pick<Config, "projects"> = {
+const config: Pick<
+	Config,
+	| "projects"
+	| "coverageReporters"
+	| "collectCoverageFrom"
+	| "coveragePathIgnorePatterns"
+> = {
 	projects: [
 		"<rootDir>/tests/unit/jest.unit.config.ts",
 		"<rootDir>/tests/integration/jest.integration.config.ts",
 	],
+	collectCoverageFrom: ["src/*.{js,ts,jsx,tsx}"],
+	coverageReporters: ["lcov", "json"],
+	coveragePathIgnorePatterns: testPathIgnorePatterns,
 };
 
 export default config;
