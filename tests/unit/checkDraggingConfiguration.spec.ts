c1730583a23059d66d9159ed198c3b7d59b2b87a
artus9033 <artus9033@gmail.com>
2024-08-15 01:43:39 +0200

    test: add unit test for checkDraggingConfiguration

---
+++ tests/unit/checkDraggingConfiguration.spec.ts
@@ -0,0 +1,307 @@
+import { Chart } from "chart.js";
+
+import { exportsForTesting } from "../../dist/chartjs-plugin-dragdata-test-browser";
+import { isTestsConfigWhitelistItemAllowed } from "../__utils__/testsConfig";
+import { setupChartInstance } from "./__utils__/utils";
+
+const { checkDraggingConfiguration, getElement } = exportsForTesting;
+
+/**
+ * The default configuration of dragging that should be calculated when
+ * no dragging options (or default values) are passed to chart
+ */
+const VANILLA_DEFAULT_CONFIG = {
+		chartDraggingDisabled: false,
+		datasetDraggingDisabled: false,
+		xAxisDraggingDisabled: true,
+		yAxisDraggingDisabled: false,
+		dataPointDraggingDisabled: false,
+	},
+	/**
+	 * The default configuration of dragging that is set up in beforeAll() call
+	 */
+	TEST_SETUP_DEFAULT_CONFIG = {
+		...VANILLA_DEFAULT_CONFIG,
+		xAxisDraggingDisabled: false,
+	};
+
+const xAxisID = "x",
+	yAxisID = "y";
+
+(isTestsConfigWhitelistItemAllowed(
+	"unit",
+	"whitelistedTestCategories",
+	"checkDraggingConfiguration",
+)
+	? describe
+	: describe.skip)("checkDraggingConfiguration", () => {
+	let chartInstance: Chart;
+
+	beforeEach(() => {
+		// mock required chart methods for unit tests to work
+		chartInstance = setupChartInstance(
+			"line",
+			{
+				plugins: {
+					// TODO: fix this later with proper TS typings
+					// @ts-ignore next line
+					dragData: {
+						round: 2,
+						dragX: true,
+						dragY: true,
+					},
+				},
+				indexAxis: "x",
+			},
+			{
+				chartData: {
+					datasets: [
+						{
+							data: [
+								{ x: 10, y: 20 },
+								{ x: 20, y: 30 },
+								{ x: 30, y: 40 },
+								{ x: 40, y: 50 },
+								{ x: 50, y: 60 },
+							],
+						},
+						{
+							data: [
+								{ x: 15, y: 25 },
+								{ x: 25, y: 35 },
+								{ x: 35, y: 45 },
+								{ x: 45, y: 55 },
+								{ x: 55, y: 65 },
+							],
+						},
+					],
+				},
+			},
+		);
+
+		const origGetDatasetMeta = chartInstance.getDatasetMeta.bind(chartInstance);
+		chartInstance.getDatasetMeta = (datasetIndex) => ({
+			...origGetDatasetMeta(datasetIndex),
+			// mock the IDs for axes
+			xAxisID,
+			yAxisID,
+		});
+
+		chartInstance.getElementsAtEventForMode = () => [
+			{
+				datasetIndex: 0,
+				index: 0,
+				element: {
+					x: 10,
+					y: 20,
+					active: true,
+					$animations: {},
+					options: {},
+					tooltipPosition: () => ({
+						x: 10,
+						y: 20,
+					}),
+					getProps() {
+						return {};
+					},
+					hasValue() {
+						return true;
+					},
+				},
+			},
+		];
+
+		// final preparation: initializes all proper variables in the dragdata plugin
+		getElement(
+			{
+				clientX: 50,
+				clientY: 200,
+				type: "click",
+			},
+			chartInstance,
+			() => {},
+		);
+	});
+
+	it("has valid default dragging configuration", () => {
+		// reset the options to the default state (beforeAll activates dragging on both axes via plugin options)
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.options.plugins.dragData = undefined;
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			VANILLA_DEFAULT_CONFIG,
+		);
+	});
+
+	it("disables all drag interactions when chart-level dragging is disabled", () => {
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.plugins.dragData = false;
+
+		const allDisabled = {
+			chartDraggingDisabled: true,
+			datasetDraggingDisabled: true,
+			xAxisDraggingDisabled: true,
+			yAxisDraggingDisabled: true,
+			dataPointDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			allDisabled,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 0, 4)).toEqual(
+			allDisabled,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 0)).toEqual(
+			allDisabled,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 4)).toEqual(
+			allDisabled,
+		);
+	});
+
+	it("disables dataset-level-and-below drag interactions when dataset dragging is disabled", () => {
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.data.datasets[0].dragData = false;
+
+		const datasetDisabledConfig = {
+			chartDraggingDisabled: false,
+			datasetDraggingDisabled: true,
+			xAxisDraggingDisabled: true,
+			yAxisDraggingDisabled: true,
+			dataPointDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			datasetDisabledConfig,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 0, 3)).toEqual(
+			datasetDisabledConfig,
+		);
+
+		// the change for dataset 0 should not affect dataset 1
+		expect(checkDraggingConfiguration(chartInstance, 1, 0)).toEqual(
+			TEST_SETUP_DEFAULT_CONFIG,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 3)).toEqual(
+			TEST_SETUP_DEFAULT_CONFIG,
+		);
+	});
+
+	it("disables only x-axis dragging when per-axis option for x-axis is disabled", () => {
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.scales[xAxisID].dragData = false;
+
+		const expectedConfig = {
+			...TEST_SETUP_DEFAULT_CONFIG,
+			xAxisDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			expectedConfig,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 3)).toEqual(
+			expectedConfig,
+		);
+	});
+
+	it("does not enable x-axis dragging when per-axis option is set to false and overrides enabled plugin option for x-axis", () => {
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.scales[xAxisID].dragData = false;
+
+		const expectedConfig = {
+			...TEST_SETUP_DEFAULT_CONFIG,
+			xAxisDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			expectedConfig,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 3)).toEqual(
+			expectedConfig,
+		);
+	});
+
+	it("disables only y-axis dragging when per-axis option for y-axis is disabled when x-axis dragging is disabled by default", () => {
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.scales[yAxisID].dragData = false;
+
+		const expectedConfig = {
+			...TEST_SETUP_DEFAULT_CONFIG,
+			yAxisDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			expectedConfig,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 3)).toEqual(
+			expectedConfig,
+		);
+	});
+
+	it("disables only y-axis dragging when per-axis option for y-axis is disabled when x-axis dragging is enabled", () => {
+		// enable dragging the x-axis in scales' options
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.scales[xAxisID].dragData = true;
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.config.options.scales[yAxisID].dragData = false;
+
+		const expectedConfig = {
+			...TEST_SETUP_DEFAULT_CONFIG,
+			xAxisDraggingDisabled: false,
+			yAxisDraggingDisabled: true,
+		};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			expectedConfig,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 3)).toEqual(
+			expectedConfig,
+		);
+	});
+
+	it("produces proper config for all axes enabled & per-data-point dragging disabled just for one point", () => {
+		// enable dragging on all axes in plugin options
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.options.plugins.dragData.dragX = true;
+		// TODO: fix this later with proper TS typings
+		// @ts-ignore
+		chartInstance.data.datasets[0].data[0].dragData = false;
+
+		const expectedConfigBase = {
+				chartDraggingDisabled: false,
+				datasetDraggingDisabled: false,
+				xAxisDraggingDisabled: false,
+				yAxisDraggingDisabled: false,
+			},
+			expectedConfigForThePoint = {
+				...expectedConfigBase,
+				dataPointDraggingDisabled: true,
+			},
+			expectedConfigOtherPoints = {
+				...expectedConfigBase,
+				dataPointDraggingDisabled: false,
+			};
+
+		expect(checkDraggingConfiguration(chartInstance, 0, 0)).toEqual(
+			expectedConfigForThePoint,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 0, 1)).toEqual(
+			expectedConfigOtherPoints,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 0)).toEqual(
+			expectedConfigOtherPoints,
+		);
+		expect(checkDraggingConfiguration(chartInstance, 1, 1)).toEqual(
+			expectedConfigOtherPoints,
+		);
+	});
+});
