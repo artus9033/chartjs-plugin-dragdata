048b46d84087bf1b3af53fa248ed3145349a4e77
artus9033 <artus9033@gmail.com>
2024-08-15 18:30:39 +0200

    refactor(test): use test.step in e2e tests, fix beforeEach run multiple times in e2e

--- tests/e2e/__fixtures__/setup.ts
+++ tests/e2e/__fixtures__/setup.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import { test } from "playwright-test-coverage";
 
 import { type Page } from "@playwright/test";
 
@@ -22,63 +23,66 @@ export async function setupE2ETest(
 	page: Page,
 	isMobile: boolean,
 ) {
-	options.disablePlugin = options.disablePlugin ?? false;
+	await test.step(`Load & initialize chart '${options.fileName}'`, async () => {
+		options.disablePlugin = options.disablePlugin ?? false;
 
-	const scenario = TestScenarios[options.fileName];
+		const scenario = TestScenarios[options.fileName];
 
-	const testChartSetupOptions: TestChartSetupOptions = {
-		...options,
-		isTest: true,
-		roundingPrecision: scenario.roundingPrecision,
-		onDrag: scenario.onDrag,
-	};
+		const testChartSetupOptions: TestChartSetupOptions = {
+			...options,
+			isTest: true,
+			roundingPrecision: scenario.roundingPrecision,
+			onDrag: scenario.onDrag,
+		};
 
-	if (isMobile) {
-		let viewport = page.viewportSize();
+		if (isMobile) {
+			let viewport = page.viewportSize();
 
-		// switch to landscape so that the chart takes full screen size
-		if (viewport?.width && viewport.width < viewport.height) {
-			await page.setViewportSize({
-				width: viewport!.height,
-				height: viewport!.width,
-			});
+			// switch to landscape so that the chart takes full screen size
+			if (viewport?.width && viewport.width < viewport.height) {
+				await page.setViewportSize({
+					width: viewport!.height,
+					height: viewport!.width,
+				});
+			}
 		}
-	}
 
-	for (const key of Object.keys(options) as (keyof SetupTestOptions)[]) {
-		switch (key) {
-			case "fileName":
-			default:
-				// skip these properties - they are spread to testChartSetupOptions above but do not have any side effects
-				continue;
+		for (const key of Object.keys(options) as (keyof SetupTestOptions)[]) {
+			switch (key) {
+				case "fileName":
+				default:
+					// skip these properties - they are spread to testChartSetupOptions above but do not have any side effects
+					continue;
 
-			case "magnet":
-				if (options.magnet) {
-					const magnetImpl = MagnetImplementations[options.magnet];
+				case "magnet":
+					if (options.magnet) {
+						const magnetImpl = MagnetImplementations[options.magnet];
 
-					if (magnetImpl) {
-						testChartSetupOptions.magnetImplSerialized = magnetImpl.toString();
+						if (magnetImpl) {
+							testChartSetupOptions.magnetImplSerialized =
+								magnetImpl.toString();
+						}
 					}
-				}
 
-				break;
+					break;
+			}
 		}
-	}
 
-	await page.goto(
-		`file://${path.dirname(__filename)}/../../../pages/dist-e2e/${options.fileName}?isTest=true`,
-	);
-	await page.waitForLoadState("load");
+		await page.goto(
+			`file://${path.dirname(__filename)}/../../../pages/dist-e2e/${options.fileName}?isTest=true`,
+		);
+		await page.waitForLoadState("load");
 
-	// run the actual setup function in browser context
-	await page.evaluate(
-		({ testChartSetupOptions }) => {
-			window.setupChart(testChartSetupOptions);
-		},
-		{ testChartSetupOptions: testChartSetupOptions as any },
-	);
+		// run the actual setup function in browser context
+		await page.evaluate(
+			({ testChartSetupOptions }) => {
+				window.setupChart(testChartSetupOptions);
+			},
+			{ testChartSetupOptions: testChartSetupOptions as any },
+		);
 
-	// since the plugin script is lazily loaded based on URL search params,
-	// the below is to stably check whether the page is ready for real
-	await page.waitForFunction(() => window.isTestReady);
+		// since the plugin script is lazily loaded based on URL search params,
+		// the below is to stably check whether the page is ready for real
+		await page.waitForFunction(() => window.isTestReady);
+	});
 }
--- tests/e2e/configurationChanges.spec.ts
+++ tests/e2e/configurationChanges.spec.ts
@@ -29,6 +29,10 @@ test.describe.configure({ mode: "parallel" });
 describeEachChartType(function testGenerator(fileName, scenario) {
 	test.describe.configure({ mode: "parallel" });
 
+	test.beforeEach("Setup page for test", async ({ page, isMobile }) => {
+		await setupE2ETest({ fileName, draggableAxis: "both" }, page, isMobile);
+	});
+
 	for (let enablerLocationSpec of ALL_ENABLER_LOCATION_SPECS.filter(
 		(enablerLocationSpec) =>
 			// to configure drag data to be disabled just for a given data sample, we need to pass an object specifying that sample
@@ -38,10 +42,6 @@ describeEachChartType(function testGenerator(fileName, scenario) {
 				(scenario.isCategoricalX || scenario.isCategoricalY)
 			),
 	)) {
-		test.beforeEach(async ({ page, isMobile }) => {
-			await setupE2ETest({ fileName, draggableAxis: "both" }, page, isMobile);
-		});
-
 		test(`data dragging behaviour changes appropriately upon ${enablerLocationSpec} dragData configuration property changes`, async ({
 			page,
 			browserName,
@@ -92,98 +92,107 @@ describeEachChartType(function testGenerator(fileName, scenario) {
 				};
 
 			async function updateChartDragDataEnabledConfig(bEnabled: boolean) {
-				await page.evaluate(
-					({ enablerLocationSpec, bEnabled, dragPointSpec }) => {
-						switch (enablerLocationSpec) {
-							case "dataset config":
-								window.testedChart.data.datasets =
-									window.testedChart.data.datasets.map((dataset) => ({
-										...dataset,
-										dragData: bEnabled,
-									}));
-								break;
+				await test.step(`Update dragdata config with '${bEnabled}' for axis: ${enablerLocationSpec}`, async () => {
+					await page.evaluate(
+						({ enablerLocationSpec, bEnabled, dragPointSpec }) => {
+							switch (enablerLocationSpec) {
+								case "dataset config":
+									window.testedChart.data.datasets =
+										window.testedChart.data.datasets.map((dataset) => ({
+											...dataset,
+											dragData: bEnabled,
+										}));
+									break;
 
-							case "data sample config":
-								{
-									const dataSample =
-										window.testedChart.data.datasets[dragPointSpec.datasetIndex]
-											.data[dragPointSpec.datasetIndex];
+								case "data sample config":
+									{
+										const dataSample =
+											window.testedChart.data.datasets[
+												dragPointSpec.datasetIndex
+											].data[dragPointSpec.datasetIndex];
 
-									if (typeof dataSample === "number") {
-										throw new IncompatibleTestConfiguration(
-											"updateChartDragDataEnabledConfig: 'data sample config' dragData enabler location specifier is incompatible with charts using a categorical axis",
-										);
+										if (typeof dataSample === "number") {
+											throw new IncompatibleTestConfiguration(
+												"updateChartDragDataEnabledConfig: 'data sample config' dragData enabler location specifier is incompatible with charts using a categorical axis",
+											);
+										}
+
+										window.testedChart.data.datasets[
+											dragPointSpec.datasetIndex
+										].data[dragPointSpec.datasetIndex] = Array.isArray(
+											dataSample,
+										)
+											? {
+													x: dataSample[0],
+													y: dataSample[1],
+													// TODO: fix this later with proper TS typings
+													// @ts-ignore
+													dragData: bEnabled,
+												}
+											: {
+													...dataSample,
+													// TODO: fix this later with proper TS typings
+													// @ts-ignore
+													dragData: bEnabled,
+												};
 									}
+									break;
 
-									window.testedChart.data.datasets[
-										dragPointSpec.datasetIndex
-									].data[dragPointSpec.datasetIndex] = Array.isArray(dataSample)
-										? {
-												x: dataSample[0],
-												y: dataSample[1],
-												// TODO: fix this later with proper TS typings
-												// @ts-ignore
-												dragData: bEnabled,
-											}
-										: {
-												...dataSample,
-												// TODO: fix this later with proper TS typings
-												// @ts-ignore
-												dragData: bEnabled,
-											};
-								}
-								break;
+								case "x-scale":
+								case "y-scale":
+									window.testedChart.config.options!.scales![
+										enablerLocationSpec === "x-scale" ? "x" : "y"
+										// TODO: fix this later with proper TS typings
+										// @ts-ignore
+									]!.dragData = bEnabled;
+									break;
+							}
 
-							case "x-scale":
-							case "y-scale":
-								window.testedChart.config.options!.scales![
-									enablerLocationSpec === "x-scale" ? "x" : "y"
-									// TODO: fix this later with proper TS typings
-									// @ts-ignore
-								]!.dragData = bEnabled;
-								break;
-						}
+							window.testedChart.update("none");
 
-						window.testedChart.update("none");
-
-						console.log(
-							"[updateChartDragDataEnabledConfig] Updated configuration:",
-							JSON.stringify(window.testedChart.config),
-						);
-					},
-					{
-						enablerLocationSpec,
-						bEnabled,
-						dragPointSpec,
-					},
-				);
+							console.log(
+								"[updateChartDragDataEnabledConfig] Updated configuration:",
+								JSON.stringify(window.testedChart.config),
+							);
+						},
+						{
+							enablerLocationSpec,
+							bEnabled,
+							dragPointSpec,
+						},
+					);
+				});
 			}
 
 			// disable dragData for a given axis
-			await updateChartDragDataEnabledConfig(false);
+			await test.step(`test dragging when disabled in config for axis: ${enablerLocationSpec}`, async () => {
+				await updateChartDragDataEnabledConfig(false);
 
-			// assert that no drag occurs when the plugin is disabled
-			await playwrightTestDrag({
-				...commonDragParams,
-				isDragDataPluginDisabled:
-					enablerLocationSpec === "data sample config" ||
-					enablerLocationSpec === "dataset config",
-				draggableAxis:
-					enablerLocationSpec === "x-scale"
-						? "y"
-						: enablerLocationSpec === "y-scale"
-							? "x"
-							: "both",
-				additionalInfo: ` (dragging disabled for ${enablerLocationSpec})`,
+				// assert that no drag occurs when the plugin is disabled
+				await playwrightTestDrag({
+					...commonDragParams,
+					isDragDataPluginDisabled:
+						enablerLocationSpec === "data sample config" ||
+						enablerLocationSpec === "dataset config",
+					draggableAxis:
+						enablerLocationSpec === "x-scale"
+							? "y"
+							: enablerLocationSpec === "y-scale"
+								? "x"
+								: "both",
+					additionalInfo: ` (dragging disabled for ${enablerLocationSpec})`,
+				});
 			});
 
-			// update the configuration to enable dragData again for the axis
-			await updateChartDragDataEnabledConfig(true);
+			await test.step(`test dragging when enabled in config for axis: ${enablerLocationSpec}`, async () => {
+				// update the configuration to enable dragData again for the axis
+				await updateChartDragDataEnabledConfig(true);
 
-			// assert that the drag occurs after configuration allows for dragging again
-			await playwrightTestDrag({
-				...commonDragParams,
-				additionalInfo: ` (dragging enabled for ${enablerLocationSpec})`,
+				// assert that the drag occurs after configuration allows for dragging again
+				await playwrightTestDrag({
+					...commonDragParams,
+					additionalInfo: ` (dragging enabled for ${enablerLocationSpec})`,
+				});
 			});
 		});
 	}
